8db1774f1a44d2e18de95cccb303db553ed0c523
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-10 15:40:53 +0100

    aggiunta immagine

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 var pageTitle = new Vue(
     {
         el : '#title',
+
         data : {
-            myTitle : 'Primi passi con VUE JS'
+            myTitle : 'Primi passi con VUE JS',
+            titleColor : 'green'
         }
+    
     }
 );
+
+
+var vuePhoto = new Vue(
+    {
+        el : '#vue-image',
+        data : {
+            divPos : 'dflex',
+            photo : 'https://www.targetweb.it/wp-content/uploads/2019/01/vuejs.png',
+            imgStyle : 'my-style'
+        }
+    }
+)
